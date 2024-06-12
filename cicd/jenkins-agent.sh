b9ed805d84aeefbf1a10e86306533b19e3711a1b
saisharanyavaidya <saisharanya.vaidya05@gmail.com>
2024-06-12 10:16:45 +0530

    commit

--- cicd/jenkins-agent.sh
+++ cicd/jenkins-agent.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-yum install fontconfig java-17-openjdk -y
+yum install fontconfig java-17-openjdk -y #java installation is must in AGENT node for Master to connect or to run any code in agent
+#installing terraform -- this will be helpful to create infra using jenkins like to run 01-vpc in expense-infra-dev project 
 yum install -y yum-utils
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 yum -y install terraform
+#installing nodejs for expense backend application
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
+yum install zip -y
--- cicd/main.tf
+++ cicd/main.tf
@@ -32,6 +32,37 @@ module "jenkins-agent" {
   } 
 }
 
+resource "aws_key_pair" "tools" {
+  key_name   = "tools"
+  # you can paste the public key directly like this
+  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
+  public_key = file("~/.ssh/tools.pub")
+  # ~ means windows home directory
+}
+
+module "nexus" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "nexus"
+
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = ["sg-0fbcae1d2d339bcf4"]
+  # convert StringList to list and get first element
+  subnet_id = "subnet-0ce3cdb1524e357ae"
+  ami = data.aws_ami.nexus_ami_info.id
+  key_name = aws_key_pair.tools.key_name
+  root_block_device = [
+    {
+      volume_type = "gp3"
+      volume_size = 30
+    }
+  ]
+  tags = {
+    Name = "nexus"
+  }
+}
+
+
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
@@ -55,6 +86,15 @@ module "records" {
       ttl = 1
       records = [
         module.jenkins-agent.private_ip
+      ]
+    },
+    {
+      name    = "nexus"
+      type    = "A"
+      ttl     = 1
+      allow_overwrite = true
+      records = [
+        module.nexus.private_ip
       ]
     }
   ]
